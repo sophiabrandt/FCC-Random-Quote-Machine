161d31fc2d3a934d6b9cad8ddc450c3f42d0193d
Sophia Brandt <sophiabrandt@users.noreply.github.com>
2016-06-22 11:02:37 +0200

    Updated at hyperdev.com

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
-(global => { // IIFE wrapper
+// IIFE wrapper
+(global => {
 'use strict';
 
 // *****************************************************
@@ -8,18 +9,43 @@
 const quoteMachine = {
   apiURL: 'https://got-quotes.herokuapp.com/quotes',
   
-  getJSON: (renderQuote, setTweetText) => $.getJSON(quoteMachine.apiURL, renderQuote).done(setTweetText),
+  // Vanilla ES6 AJAX function via http://www.html5rocks.com/en/tutorials/es6/promises/
+  ajax: function (url) {
+  return new Promise(function(resolve, reject) {
+    const req = new XMLHttpRequest();
+    req.open('GET', url);
+    req.onload = function() {
+      if (req.status === 200) {
+        const res = req.responseText;
+        resolve(JSON.parse(res));
+         }
+      reject;
+      };
+    req.onerror = reject;
+    req.send();
+    });
+  },
   
-  renderQuote: ({quote, character}) => view.render(quote, character),
+  getJSON: function(renderQuote, setTweetText) {
+    quoteMachine.ajax(quoteMachine.apiURL)
+      .then(renderQuote)
+      .then(setTweetText)
+      .catch(err => {console.log(err)});
+  },
+  
+  renderQuote: function(JSON) {
+    view.render(JSON);
+    return JSON;
+  },
     
-  setTweetText: ({quote, character}) => {
+  setTweetText: function ({quote, character}) {
     const tweetIntent = 'https://twitter.com/intent/tweet?hashtags=gameofthrones&text=';
     const normalTweet = quote + ' ~ ' + character;
     const shortenedTweet = normalTweet.substr(0,122) + '...';
     if (normalTweet.length > 140) {
       return handlers.tweetButton(tweetIntent+shortenedTweet);
         }
-    return handlers.tweetButton(tweetIntent+normalTweet);
+      return handlers.tweetButton(tweetIntent+normalTweet);
   }
 };
 
@@ -28,19 +54,21 @@ const quoteMachine = {
 // *****************************************************
 
 const handlers = {
-  randomizeButton: () => {
+  randomizeButton: function() {
       const randomizeButton = document.querySelector('#randomizeButton');
       randomizeButton.addEventListener('click', handlers.getRandomQuote);
     },
   
-  tweetButton: (tweetText) => {
+  tweetButton: function (tweetText) {
     const tweetButton = document.querySelector('#tweetButton');
     tweetButton.addEventListener('click', ()  => {tweetButton.href = tweetText});
   },
   
-  getRandomQuote: () => quoteMachine.getJSON(quoteMachine.renderQuote, quoteMachine.setTweetText),
+  getRandomQuote: function() {
+    quoteMachine.getJSON(quoteMachine.renderQuote, quoteMachine.setTweetText);
+  },
 
-  init: () => {
+  init: function() {
     handlers.getRandomQuote();
     handlers.randomizeButton();
     },
@@ -51,7 +79,7 @@ const handlers = {
 // *****************************************************
 
 const view = {
-  render: (quote, character) => {
+  render: function({quote, character}) {
     const displayQuote = document.querySelector('#displayQuote');
     const displaySource = document.querySelector('#displayCharacter');
     displayQuote.textContent = quote;
