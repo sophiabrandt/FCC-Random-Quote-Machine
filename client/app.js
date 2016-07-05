4a4dc6579210a8de70d51a9575a818544dc12631
Sophia Brandt <sophiabrandt@users.noreply.github.com>
2016-07-05 06:40:39 +0200

    Updated at hyperdev.com

--- client/app.js
+++ client/app.js
@@ -10,7 +10,7 @@ const quoteMachine = {
   apiURL: 'https://got-quotes.herokuapp.com/quotes',
   
   // Vanilla ES6 AJAX function via http://www.html5rocks.com/en/tutorials/es6/promises/
-  ajax: function (url) {
+  ajax(url) {
   return new Promise(function(resolve, reject) {
     const req = new XMLHttpRequest();
     req.open('GET', url);
@@ -26,19 +26,19 @@ const quoteMachine = {
     });
   },
   
-  getJSON: function(renderQuote, setTweetText) {
+  getJSON(renderQuote, setTweetText) {
     quoteMachine.ajax(quoteMachine.apiURL)
       .then(renderQuote)
       .then(setTweetText)
       .catch(err => {console.log(err)});
   },
   
-  renderQuote: function(JSON) {
+  renderQuote(JSON) {
     view.render(JSON);
     return JSON;
   },
     
-  setTweetText: function ({quote, character}) {
+  setTweetText({quote, character}) {
     const tweetIntent = 'https://twitter.com/intent/tweet?hashtags=gameofthrones&text=';
     const normalTweet = quote + ' ~ ' + character;
     const shortenedTweet = normalTweet.substr(0,122) + '...';
@@ -54,21 +54,21 @@ const quoteMachine = {
 // *****************************************************
 
 const handlers = {
-  randomizeButton: function() {
+  randomizeButton() {
       const randomizeButton = document.querySelector('#randomizeButton');
       randomizeButton.addEventListener('click', handlers.getRandomQuote);
     },
   
-  tweetButton: function (tweetText) {
+  tweetButton(tweetText) {
     const tweetButton = document.querySelector('#tweetButton');
     tweetButton.addEventListener('click', ()  => {tweetButton.href = tweetText});
   },
   
-  getRandomQuote: function() {
+  getRandomQuote() {
     quoteMachine.getJSON(quoteMachine.renderQuote, quoteMachine.setTweetText);
   },
 
-  init: function() {
+  init() {
     handlers.getRandomQuote();
     handlers.randomizeButton();
     },
@@ -79,7 +79,7 @@ const handlers = {
 // *****************************************************
 
 const view = {
-  render: function({quote, character}) {
+  render({quote, character}) {
     const displayQuote = document.querySelector('#displayQuote');
     const displaySource = document.querySelector('#displayCharacter');
     displayQuote.textContent = quote;
